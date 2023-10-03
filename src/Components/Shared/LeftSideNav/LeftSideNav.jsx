8e1bf32259ea3c4d6664263178e550c8937cb188
omarjafor <omarjaforchy@gmail.com>
2023-10-03 15:50:39 +0600

    categories data load

--- src/Components/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 
 
 const LeftSideNav = () => {
+
+    const [category, setCategory] = useState([]);
+
+    useEffect( () => {
+        fetch('./categories.json')
+        .then(res => res.json())
+        .then(data => setCategory(data))
+    } , [])
     return (
         <div>
             <h1 className="text-3xl font-bold">
-                All Caterogy
+                All Category : {category.length}
             </h1>
         </div>
     );
