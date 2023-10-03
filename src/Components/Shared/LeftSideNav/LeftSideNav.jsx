24de06839ed6f386cde1c44ccaa2e954f182a76d
omarjafor <omarjaforchy@gmail.com>
2023-10-03 16:17:17 +0600

    categories data load

--- src/Components/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Components/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
@@ -11,10 +12,15 @@ const LeftSideNav = () => {
         .then(data => setCategory(data))
     } , [])
     return (
-        <div>
-            <h1 className="text-3xl font-bold">
-                All Category : {category.length}
-            </h1>
+        <div className="space-y-6">
+            <h1 className="text-3xl font-bold"> All Categories </h1>
+            {
+                category.map(cate => <Link 
+                    className="block ml-4 text-xl font-semibold"
+                    key={cate.id}
+                    to={`/category/${cate.id}`}
+                    > {cate.name} </Link>)
+            }
         </div>
     );
 };
