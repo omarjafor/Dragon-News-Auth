5d00d29e4d32f5f02ef06adda5f8d6b7ad32b3b5
omarjafor <omarjaforchy@gmail.com>
2023-10-03 20:13:49 +0600

    login register page design

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,53 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log('Hellooooooo Register')
+    }
     return (
         <div>
-            <h1 className="text-3xl font-bold">
-                Please Register First
-            </h1>
+            <Navbar></Navbar>
+            <div>
+                <h2 className="text-3xl my-10 text-center">Please Register</h2>
+                <form onSubmit={handleRegister} className=" md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" required name="name" placeholder="Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" required name="photo" placeholder="Photo URL" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" required name="password" placeholder="Password" className="input input-bordered" />
+                        <label className="mt-2 border">
+                            <input type="checkbox" name="terms" id="" />
+                            <a href="#" className=" text-[#403F3F]"> Accept Term & Conditions</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+                <p className="text-center mt-4">Already have an account? <Link className="text-blue-600 font-bold" to="/login">Login</Link></p>
+            </div>
         </div>
     );
 };
