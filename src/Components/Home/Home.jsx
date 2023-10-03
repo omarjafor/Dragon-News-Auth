4a0ece56bff3a9315563e17e29e56586cff4a31c
omarjafor <omarjaforchy@gmail.com>
2023-10-03 15:41:58 +0600

    right side design

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,14 +10,14 @@ const Home = () => {
             <Header></Header>
             <BreakingNews></BreakingNews>
             <Navbar></Navbar>
-            <div className="grid border grid-cols-1 md:grid-cols-4 gap-6 mt-20">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-20">
                 <div className="border">
                     <LeftSideNav></LeftSideNav>
                 </div>
                 <div className="md:col-span-2 border">
                     <h1 className="text-3xl font-bold">Dragon News Home</h1>
                 </div>
-                <div className="border">
+                <div className="">
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
--- src/Components/Shared/RightSideNav/RightSideNav.jsx
+++ src/Components/Shared/RightSideNav/RightSideNav.jsx
@@ -1,4 +1,8 @@
 import { BsGoogle, BsGithub, BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+import qzone1 from '../../../images/qZone1.png';
+import qzone2 from '../../../images/qZone2.png';
+import qzone3 from '../../../images/qZone3.png';
+
 
 const RightSideNav = () => {
     return (
@@ -28,6 +32,19 @@ const RightSideNav = () => {
                     <BsInstagram></BsInstagram>
                     <span className='ml-2'>Instagram</span>
                 </a>
+            </div>
+            <div className="p-4 space-y-3 mb-3 bg-[#F3F3F3]">
+                <h2 className="text-3xl">Q-Zone</h2>
+               <img src={qzone1} alt="" />
+               <img src={qzone2} alt="" />
+               <img src={qzone3} alt="" />
+            </div>
+            <div className="p-10 mt-5 text-center space-y-3 mb-3 bg-[url('./images/bg.png')]">
+                <h2 className="text-3xl font-bold leading-10 text-white mt-6">Create an Amazing Newspaper</h2>
+                <p className='text-white'>
+                    Discover thousands of options, easy to customize layouts, one-click to import demo and much more.
+                </p>
+                <button className='px-5 py-4 text-white bg-[#D72050]'>Learn More</button>
             </div>
         </div>
     );
