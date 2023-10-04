8732ca5f2fa522250c5be9ae1e508230245cf6e5
omarjafor <omarjaforchy@gmail.com>
2023-10-04 12:41:08 +0600

    news private loading

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -7,11 +7,13 @@ export const AuthContext = createContext(null);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect( () => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('User in the auth state changed', currentUser)
             setUser(currentUser);
+            setLoading(false);
         });
 
         return () => {
@@ -20,18 +22,21 @@ const AuthProvider = ({children}) => {
     } , [])
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const logOut = () => {
+        setLoading(true);
        return signOut(auth)
     }
 
-    const authInfo = { user, signIn, logOut, createUser }
+    const authInfo = { user, signIn, logOut, createUser, loading }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
