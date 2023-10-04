7171f307657714336faeb4e8e2c4c0fba7253660
omarjafor <omarjaforchy@gmail.com>
2023-10-04 13:08:42 +0600

    news private loading

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ( {children} ) => {
     
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
+
+    const location = useLocation();
+    console.log(location);
+
+    if(loading){
+        return <span className="loading loading-spinner loading-lg"></span>;
+    }
 
     if(user){
         return children;
