ce6524f51e7ccd2c99bc481ef571b3225f8819cc
omarjafor <omarjaforchy@gmail.com>
2023-10-04 13:22:43 +0600

    firebase config hide

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
@@ -6,6 +6,8 @@ import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -16,6 +18,8 @@ const Login = () => {
         signIn(email, password)
         .then( result => {
             console.log(result.user);
+
+            navigate(location?.state ? location.state : '/');
         })
         .catch(err => {
             console.log(err.message);
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -8,7 +8,7 @@ const PrivateRoute = ( {children} ) => {
     const { user, loading } = useContext(AuthContext);
 
     const location = useLocation();
-    console.log(location);
+    console.log(location.pathname);
 
     if(loading){
         return <span className="loading loading-spinner loading-lg"></span>;
@@ -18,7 +18,7 @@ const PrivateRoute = ( {children} ) => {
         return children;
     }
 
-    return <Navigate to='/login'></Navigate>;
+    return <Navigate state={location.pathname} to='/login'></Navigate>;
 };
 
 export default PrivateRoute;
