fe9c2098eec3f76e53a6625aa68623c7b285fe9c
omarjafor <omarjaforchy@gmail.com>
2023-10-08 17:18:13 +0600

    auth practice

--- src/Components/Home/BreakingNews.jsx
+++ src/Components/Home/BreakingNews.jsx
@@ -6,7 +6,7 @@ const BreakingNews = () => {
         <div className="flex">
             <button className="btn text-white hover:text-black bg-[#D72050]">Latest</button>
             <Marquee pauseOnHover={true} style={ { color: 'var(--Dark-02, #403F3F)', fontSize: '18px', fontWeight: 600, lineHeight: '30px'} }>
-                <Link to='/'> I can be a React component, multiple React components, or just some text.</Link>
+                <Link to='/'> I can be a , multiple React components, or just some text.</Link>
             </Marquee>
         </div>
     );
